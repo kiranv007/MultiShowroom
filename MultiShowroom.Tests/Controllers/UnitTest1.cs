41c2fe3eb0af33d8cef9e9bde7e548669a4986a6
kiranv007 <Kiran.V@nttdata.com>
2024-04-22 14:12:47 +0530

    updated images

---
+++ MultiShowroom.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiShowroom.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace MultiShowroom.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            xunittestController controller = new xunittestController();
+
+            // Act 
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert 
+            Assert.AreEqual("Yash", result.ViewBag.Sample);
+        }
+    }
+}
---
+++ MultiShowroom/Controllers/xunittestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiShowroom.Controllers
+{
+    public class xunittestController : Controller
+    {
+        // GET: xunittest
+        public ActionResult Index()
+        {
+            ViewBag.Sample = "Yash";
+            return View();
+        }
+    }
+}
